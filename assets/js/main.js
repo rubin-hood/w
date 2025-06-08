f99946aa019e446964e4abf1d8315d67729792ca
Rubin Hood <andreasrubin1980@gmail.com>
2025-06-08 20:37:45 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+console.log("main.js geladen!");
+
+
 document.addEventListener("DOMContentLoaded", function() {
   var sidebar = document.getElementById("sidebar");
   var toggle = document.getElementById("sidebar-toggle");
